c6687f8c7fa44ccc4f4e574867ce8cf53675f327
Mathieu Lecarme <mlecarme@bearstech.com>
2017-01-04 22:41:53 +0100

    Fix: First serve

--- app/app.go
+++ app/app.go
@@ -41,6 +41,8 @@ func (a *Application) Start() error {
 		return err
 	}
 
+	a.Serve()
+
 	cc := make(chan os.Signal, 1)
 	signal.Notify(cc, os.Interrupt, syscall.SIGHUP, syscall.SIGUSR1, syscall.SIGTERM)
 	go func() {
