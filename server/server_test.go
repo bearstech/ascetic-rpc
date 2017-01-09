d629b035f3a4dae649de0055a5c5932548079ee4
Wilfried OLLIVIER <wollivier@bearstech.com>
2017-01-09 14:36:56 +0100

    Add timout unit test

--- server/server_test.go
+++ server/server_test.go
@@ -7,7 +7,30 @@ import (
 
 	"github.com/bearstech/ascetic-rpc/client"
 	"github.com/bearstech/ascetic-rpc/message"
+	"time"
 )
+
+func timedHello(req *message.Request) (*message.Response, error) {
+	var hello message.Hello
+	err := req.GetBody(&hello)
+	if err != nil {
+		panic(err)
+	}
+
+	world := message.World{
+		Message: fmt.Sprintf("Hello %s♥️", hello.Name),
+	}
+
+	fmt.Println("Timed Hello func, waiting for 13 seconds... be patient")
+	time.Sleep(13 * time.Second)
+
+	res, err := message.NewOKResponse(&world)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
 
 func hello(req *message.Request) (*message.Response, error) {
 	var hello message.Hello
@@ -32,6 +55,90 @@ func TestHelloServer(t *testing.T) {
 		t.Error(err)
 	}
 	s.Register("hello", hello)
+	serverStopped := false
+	go func() {
+		s.Serve()
+		serverStopped = true
+	}()
+
+	c, err := client.NewClientUnix(socketPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	hello := message.Hello{Name: "Alice"}
+	var world message.World
+
+	err = c.Do("hello", &hello, &world)
+	if err != nil {
+		t.Error(err)
+	}
+	err = c.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	if world.Message != "Hello Alice♥️" {
+		t.Error(errors.New("Bad message: " + world.Message))
+	}
+	s.Stop()
+	if !serverStopped {
+		t.Error(errors.New("Bad stop"))
+	}
+	if s.IsRunning() {
+		t.Error(errors.New("Ghost running"))
+	}
+}
+
+func TestTimeoutServer(t *testing.T) {
+	socketPath := "/tmp/test.sock"
+	s, err := NewServerUnix(socketPath)
+	if err != nil {
+		t.Error(err)
+	}
+	// timeout set to 1 sec, timedHello() duration is 10 sec
+	// in this case, cut the wire.
+	s.Register("hello", timedHello).WithTimeout(1 * time.Second)
+	serverStopped := false
+	go func() {
+		s.Serve()
+		serverStopped = true
+	}()
+
+	c, err := client.NewClientUnix(socketPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	hello := message.Hello{Name: "Alice"}
+	var world message.World
+
+	err = c.Do("hello", &hello, &world)
+	if err == nil {
+		t.Error(errors.New("Timeout expected"))
+	}
+	err = c.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	if world.Message != "Hello Alice♥️" {
+		t.Error(errors.New("Bad message: " + world.Message))
+	}
+	s.Stop()
+	if !serverStopped {
+		t.Error(errors.New("Bad stop"))
+	}
+	if s.IsRunning() {
+		t.Error(errors.New("Ghost running"))
+	}
+}
+
+func TestTimedHelloServer(t *testing.T) {
+	socketPath := "/tmp/test.sock"
+	s, err := NewServerUnix(socketPath)
+	if err != nil {
+		t.Error(err)
+	}
+	s.Register("hello", timedHello).WithTimeout(14 * time.Second)
 	serverStopped := false
 	go func() {
 		s.Serve()
